b363d9f5e8e8f6d4246638a71684d315ebd0b1b4
unknown <adibmaknun16@gmail.com>
2022-06-04 12:54:07 +0700

    add API Request experience

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,14 @@ const express = require("express");
 
 const Router = express.Router();
 
+const experienceRoutes = require("../modules/experience/experienceRoutes");
+// const hireRoutes = require("../modules/hire/hireRoutes");
+// const portfolioRoutes = require("../modules/portfolio/portfolioRoutes");
+// const skillRoutes = require("../modules/skill/skillRoutes");
+
+Router.use("/experience", experienceRoutes);
+// Router.use("/hire", hireRoutes);
+// Router.use("/portfolio", portfolioRoutes);
+// Router.use("/skill", skillRoutes);
+
 module.exports = Router;
